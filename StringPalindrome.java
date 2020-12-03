05abddef1c264e4c91b75f13f16bbe1618e48471
debamitra mukherjee <debamitra.mukherjee@gmail.com>
2020-12-03 19:27:58 +0530

    Create StringPalindrome.java

---
+++ StringPalindrome.java
@@ -0,0 +1,12 @@
+ 
+ Class StringPalindrome {
+ int isPlaindrome(String S) {
+        
+	    for(int i =0,j=S.length()-1; i<S.length()/2;i++,j--){
+	        if(S.charAt(i) != S.charAt(j)){
+	            return 0;
+	        }
+	    }
+	    return 1;
+	}
+  };
